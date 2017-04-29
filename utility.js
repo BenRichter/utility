0d0c332680163a90d2cbb37388351fd74e261795
Ben Richter <ben.k.richter@gmail.com>
2017-04-29 13:10:23 +0200

    Add unique arrays function

--- utility.js
+++ utility.js
@@ -3,8 +3,7 @@
  *
  * @param {string} num  - number to pad with zeros
  * @param {int}    size - length of number
- *
- * @return {string} padded number string
+ * @return {string}     - padded number string
  */
 export const pad = (num, size = 2) => {
     let paddedNum = `${num}`;
@@ -13,29 +12,29 @@ export const pad = (num, size = 2) => {
         paddedNum = `0${paddedNum}`;
     }
     return paddedNum;
-}
+};
+
 
 /**
  * Format date to dd.mm.yyyy, hh:mm
  *
  * @param {string}  dateString - iso string date
- *
- * @return {string} formatted string
+ * @return {string}            - formatted string
  */
 export const formatDate = (dateString) => {
     const date = new Date(dateString);
 
     return `${pad(date.getDate())}.${pad(date.getMonth())}.${date.getFullYear()},
             ${pad(date.getHours())}:${pad(date.getMinutes())}`;
-}
+};
+
 
 /**
  * Format Money with point delimiter and fixed to two floats
  *
  * @param {number} price - convert to currency string
  * @param {string} sign  - (optional) currency sign € to be appended
- *
- * @return {string} currency string
+ * @return {string}      - currency string
  */
 export const formatMoney = (price, sign = '') => {
     const pieces = parseFloat(price).toFixed(2).replace('.', ',').split('');
@@ -46,4 +45,14 @@ export const formatMoney = (price, sign = '') => {
     }
 
     return pieces.join('') + sign;
-}
+};
+
+
+/**
+ * Clear arrays of any type from duplicate entries
+ * https://twitter.com/_ericelliott/status/832065642987524096?lang=de
+ *
+ * @param {Array} arr - array with duplication [1,1,3]
+ * @return {Array}    - new array without duplications [1,3]
+ */
+export const unique = (arr) => [...new Set(arr)];
